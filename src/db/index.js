35b514e7e493ec88661598a621b8745dc98df6fc
abhishekdhull <46604435+abhishekdhull21@users.noreply.github.com>
2025-05-23 00:51:52 +0530

    env config changed

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host:  process.env.MYSQL_HOST || process.env?.__NEXT_PRIVATE_STANDALONE_CONFIG?.env?.MYSQL_HOST ,
+  port:  process.env.MYSQL_PORT || process.env?.__NEXT_PRIVATE_STANDALONE_CONFIG?.env?.MYSQL_PORT,
+  user:  process.env.MYSQL_USER || process.env?.__NEXT_PRIVATE_STANDALONE_CONFIG?.env?.MYSQL_USER,
+  password:  process.env.MYSQL_PASSWORD || process.env?.__NEXT_PRIVATE_STANDALONE_CONFIG?.env?.MYSQL_PASSWORD,
+  database:  process.env.MYSQL_DATABASE || process.env?.__NEXT_PRIVATE_STANDALONE_CONFIG?.env?.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
