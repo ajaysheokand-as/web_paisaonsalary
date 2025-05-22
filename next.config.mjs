af1120010a71a3b493188a4ec12611cc7c6c39b3
abhishekdhull <46604435+abhishekdhull21@users.noreply.github.com>
2025-05-22 19:57:35 +0530

    env added to config

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
     ],
   },
   transpilePackages: ["lucide-react"],
+    env: {
+    // server-only, baked into your SSR/API Lambdas
+    MYSQL_PORT: process.env.MYSQL_PORT,
+    MYSQL_USER: process.env.MYSQL_USER,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    // public, available in both server & client bundles
+    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,12 @@
 // src/db/index.js
 import mysql from 'mysql2/promise';
-import {  secret } from '@aws-amplify/backend';
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || secret('MYSQL_HOST'),
-  port: process.env.MYSQL_PORT || secret('MYSQL_PORT'),
-  user: process.env.MYSQL_USER || secret('MYSQL_USER'),
-  password: process.env.MYSQL_PASSWORD || secret('MYSQL_PASSWORD'),
-  database: process.env.MYSQL_DATABASE || secret('MYSQL_DATABASE'),
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
